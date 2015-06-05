d026e01ab15d256941dd43ee9862212a6eaf4ac4
Wayne Allan <wayne.allan@imaginatelabs.com>
2015-06-05 22:51:24 +1000

    Add default build command

--- spec/core/conf/default_config_spec.rb
+++ spec/core/conf/default_config_spec.rb
@@ -10,5 +10,10 @@ describe ".cxconf" do
     it "has the version 1.0.0" do
       expect(subject.version).to eq("1.0.0")
     end
+
+    it "has a default build command" do
+      expect(subject.build.cmd).to eq("echo 'Please configure your build with the property "\
+                                      "build.cmd in your project's .cxconf configuration file.'")
+    end
   end
 end
